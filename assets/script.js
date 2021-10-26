967ecd0ad8481a16bdddfb07e9439743cbde80c6
Hayley McHugh <hayleym522@gmail.com>
2021-10-25 19:05:24 -0700

    first API javascript

---
+++ assets/script.js
@@ -0,0 +1,74 @@
+var userFormEl = document.querySelector('#user-form');
+var recipeInputEl = document.querySelector('#recipe');
+var recipesContainerEL = document.querySelector('#recipes-container');
+var recipesSearchTerm = document.querySelector('#recipe-search-term');
+
+var formSubmitHandler = function (event) {
+    event.preventDefault();
+
+    var recipe = recipeInputEl.value.trim();
+
+    if (recipe) {
+      getUserRecipes(recipe);
+
+      recipesContainerEL.textContent = '';
+      recipeInputEl.value = '';
+    } else {
+        alert('Please enter a recipe');
+    }
+};
+
+var getUserRecipes = function (user) {
+    var apiURL = 'https://api.spoonacular.com/recipes/complexSearch?apiKey=1bccdbd99014422bbed7295eb22db074';
+
+    fetch(apiURL)
+      .then(function (response) {
+        if (response.ok) {
+          response.json().then(function (data) {
+            displayRecipes(data, user);
+          });
+        } else {
+            alert('Error: ' + response.statusText);
+        }
+      })
+      .catch(function (error) {
+        alert('Unable to connect');
+      });
+};
+
+var displayRecipes = function (recipes, searchTerm) {
+    if (recipes.length === 0) {
+        recipesContainerEL.textContent = 'No recipes found.';
+        return;
+    }
+
+recipesSearchTerm.textContent = searchTerm;
+
+for (var i = 0; i < recipes.length; i++) {
+    var recipeName = recipes[i];
+
+    var recipesEl = document.createElement('div');
+    recipesEl.classList = 'list-item flex-row justify-space-between align-center';
+
+    var titleEL = document.createElement('span');
+    titleEL.textContent = recipeName;
+
+    recipesEl.appendChild(titleEL);
+
+    var statusEl = document.createElement('span');
+    statusEl.classList = 'flex-row align-center';
+
+if (recipes[i] > 0) {
+    statusEl.innerHTML = 
+    "<i class='fas fa-times status-icon icon-danger'></i>" + recipes[i];
+} else {
+    statusEl.innerHTML = "<i class'fas fa-check-square status-icon icon-success'></i>";
+}
+
+    recipesEl.appendChild(statusEl);
+
+    recipesContainerEL.appendChild(recipesEl);
+    }
+};
+
+userFormEl.addEventListener('submit', formSubmitHandler); 
