a62b0627bbe14351f66d808e27025b82fd0a55a5
Hayley McHugh <hayleym522@gmail.com>
2021-10-30 14:17:08 -0700

    added comments to all three javascript files

--- assets/script2.js
+++ assets/script2.js
@@ -1,8 +1,10 @@
+// variables
 var userFormEl = document.querySelector('#user-form');
 var wineInputEl = document.querySelector('#wine');
 var wineContainerEL = document.querySelector('#wine-container');
 var wineSearchTerm = document.querySelector('#wine-search-term');
 
+// form to enter a food dish for wine pairing
 var formSubmitHandler = function (event) {
     event.preventDefault();
 
@@ -17,10 +19,11 @@ var formSubmitHandler = function (event) {
         alert('Please enter a food item');
     }
 };
-
+// wine api
 var getWine = function(searchUserInput) {
     var apiURL = `https://api.spoonacular.com/food/wine/pairing?apiKey=1bccdbd99014422bbed7295eb22db074&food=${searchUserInput}`;
 
+    // fetch and .then functions
     fetch(apiURL)
       .then(function (response) {
         return response.json();
@@ -28,14 +31,17 @@ var getWine = function(searchUserInput) {
       .then(function (data) {
         console.log(data);
         for(var i = 0; i < data.productMatches.length; i++) {
+          // created elements
           var wineTitle = document.createElement('p');
           var wineImg = document.createElement('img');
           var wineLink = document.createElement('a');
           var wineDescription = document.createElement('p');
+          // assigned data to those elements
           wineTitle.textContent=data.productMatches[i].title;
           wineImg.setAttribute("src", data.productMatches[i].imageUrl);
           wineLink.setAttribute("href", data.productMatches[i].link);
           wineDescription.textContent=data.productMatches[i].description;
+          // appended data to wine container
           wineContainerEL.appendChild(wineTitle);
           wineLink.appendChild(wineImg);
           wineContainerEL.appendChild(wineLink);
--- assets/script3.js
+++ assets/script3.js
@@ -1,26 +1,31 @@
-// Variables for id's of finalscore, reset button, and back button.
+// variables
 var savedRecipesContainerEl = document.querySelector("#saved-recipes-container");
 var clear = document.querySelector("#clearBtn");
 
-// Function to clear scores from local storage.
+// function to clear recipes from local storage
 clear.addEventListener("click", function () {
  localStorage.clear();
  location.reload();
 });
 
+// grabbing items from local storage
 var savedRecipes = localStorage.getItem("savedRecipes");
 savedRecipes = JSON.parse(savedRecipes);
 
+// if statement and for loop to grab saved recipes from local storage and loop through them
 if (savedRecipes.length !== 0) {
  for (var i = 0; i < savedRecipes.length; i++) {
+   //  created elements
     var createP = document.createElement("p");
     var createImg = document.createElement("img");
     var createLink = document.createElement("a");
     
+   //  assigned elements data from local storage object
     createP.textContent = savedRecipes[i].title;
     createImg.setAttribute("src", savedRecipes[i].image);
     createLink.setAttribute("href", savedRecipes[i].link);
     
+   //  appended data to saved recipes container
     savedRecipesContainerEl.appendChild(createP);
     savedRecipesContainerEl.appendChild(createLink);
     createLink.appendChild(createImg);
