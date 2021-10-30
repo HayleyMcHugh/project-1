534ad414994e543ab5609c449c7c9c075e84f952
LATOMMYLA <tommyl.dmd@gmail.com>
2021-10-30 11:38:12 -0700

    script3 structure

--- assets/script3.js
+++ assets/script3.js
@@ -1,6 +1,29 @@
-var savedRecipesContainerEL = document.getElementById('saved-recipes-container');
+// Variables for id's of finalscore, reset button, and back button.
+var savedRecipesContainerEl = document.querySelector("#saved-recipes-container");
+var clear = document.querySelector("#clearBtn");
 
-var displaySavedRecipes = () => {
-    let mySavedRecipes = JSON.parse(localStorage.getItem("savedRecipes")) || [];
-    savedRecipesContainerEL.appendChild(mySavedRecipes);
+// Function to clear scores from local storage.
+clear.addEventListener("click", function () {
+ localStorage.clear();
+ location.reload();
+});
+
+var savedRecipes = localStorage.getItem("savedRecipes");
+savedRecipes = JSON.parse(savedRecipes);
+
+if (savedRecipes !== null) {
+ for (var i = 0; i < savedRecipes.length; i++) {
+  var createP = document.createElement("p");
+  var createImg = document.createElement("img");
+  var createLink = document.createElement("a");
+
+  createP.textContent = savedRecipes[i];
+
+  createImg.setAttribute("src", savedRecipes[i]);
+  createLink.setAttribute("href", savedRecipes[i]);
+
+  savedRecipesContainerEl.appendChild(createP);
+  savedRecipesContainerEl.appendChild(createImg);
+  savedRecipesContainerEl.appendChild(createLink);
  }
+};
