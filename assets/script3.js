e391bd444060e59d064a558cc9c2673aa1e76bce
Hayley McHugh <hayleym522@gmail.com>
2021-10-30 11:45:08 -0700

    java3

--- assets/script3.js
+++ assets/script3.js
@@ -1,6 +1,6 @@
 var savedRecipesContainerEL = document.getElementById('saved-recipes-container');
 
 var displaySavedRecipes = () => {
-    let mySavedRecipes = JSON.parse(localStorage.getItem("savedRecipes")) || [];
+    let mySavedRecipes = JSON.parse(window.localStorage.getItem("savedRecipes")) || [];
     savedRecipesContainerEL.appendChild(mySavedRecipes);
  }
